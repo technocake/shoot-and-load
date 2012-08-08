175f4c782748d4f407f2633508fbbf4c64782850
Robin G. Aaberg <robin.garen@gmail.com>
2012-08-08 13:52:39 +0200

    jubel

--- setup.sh
+++ setup.sh
@@ -2,4 +2,15 @@
 cd ~
 git clone git@github.com:technocake/shoot-and-load.git -b marte
 
-echo "alias sendbilder='. ~/shoot-and-load/shoot-and-load.py'" >> ~/.bash_aliases
+echo "alias sendbilder='python ~/shoot-and-load/shoot-and-load.py'" >> ~/.bash_aliases
+. .bash_aliases
+
+echo "Starting sendbilder :)"
+sendbilder
+echo "Setup has finished"
+
+echo 
+echo " To start it another time, just type this command in your terminal:"
+echo "sendbilder"
+
+echo "Robin loves Marte"
