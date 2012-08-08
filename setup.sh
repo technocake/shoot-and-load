afe891c3d54f6373ee226e8eeba4ad8e14922482
Robin G. Aaberg <robin.garen@gmail.com>
2012-08-08 14:10:53 +0200

    funny

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ~
-git clone git@github.com:technocake/shoot-and-load.git -b marte
+git clone https://github.com/technocake/shoot-and-load.git -b marte
 
 echo "alias sendbilder='python ~/shoot-and-load/shoot-and-load.py'" >> ~/.bash_aliases
 . .bash_aliases
